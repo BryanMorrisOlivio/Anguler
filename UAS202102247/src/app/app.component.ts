005f07b0ee86f459051424df726c081ffc8904cd
BryanMorrisOlivio <morris.olivio@gmail.com>
2023-01-17 20:29:22 +0700

    Hasil Uas

---
+++ UAS202102247/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+showTambahModal() {
+throw new Error('Method not implemented.');
+}
+postRecord() {
+throw new Error('Method not implemented.');
+}
+  title = 'UAS202102247';
+}
